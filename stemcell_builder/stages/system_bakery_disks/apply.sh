22d0f2671f4f856afe3fa70346bb3d4dc9c358f7
Ruurd Keizer <ruurdk@hotmail.com>
2018-02-13 23:59:22 +0100

    no chkconfig

--- stemcell_builder/stages/system_bakery_disks/apply.sh
+++ stemcell_builder/stages/system_bakery_disks/apply.sh
@@ -9,7 +9,10 @@ echo "Installing open-iscsi NFS mount hack script"
 cp $assets_dir/open-iscsi $chroot/etc/init.d/open-iscsi
 
 echo "Add open-iscsi to startup"
-run_in_chroot $chroot "chkconfig --level 345 open-iscsi"
+run_in_chroot $chroot "ln -s $chroot/etc/init.d/open-iscsi $chroot/etc/rc2.d/S10open-iscsi"
+run_in_chroot $chroot "ln -s $chroot/etc/init.d/open-iscsi $chroot/etc/rc3.d/S10open-iscsi"
+run_in_chroot $chroot "ln -s $chroot/etc/init.d/open-iscsi $chroot/etc/rc4.d/S10open-iscsi"
+run_in_chroot $chroot "ln -s $chroot/etc/init.d/open-iscsi $chroot/etc/rc5.d/S10open-iscsi"
 
 echo "Add open-iscsi script to crontab"
 run_in_chroot $chroot "
